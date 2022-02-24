4ec025e79ea3e150e1c48722004c686044b63ef9
Adrian Bercovici <adriansan_93@yahoo.com>
2022-02-24 21:35:19 +0200

    added the prod

--- relscript1.sh
+++ relscript1.sh
@@ -5,5 +5,5 @@ port=${2:-8300}
 release_name=${3:- "ex_banking_dev-0.1.0"}
 folder=${4:- "./prod1/ex_banking_dev/bin/ex_banking_dev-0.1.0"}
 echo "Using node: $node on port: $port for release: $release_name"
-rebar3 release as prod1 
+rebar3 release as prod1 -o prod1
 NODE_NAME=${node}  PORT=${port} $folder console
