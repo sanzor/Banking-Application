9c1c5f93a574ef5319272d20542620e6501a93fc
Adrian Bercovici <adriansan_93@yahoo.com>
2022-03-01 16:56:30 +0200

    clean

---
+++ prod1/ex_banking/lib/ex_banking-0.1.0/src/ex_banking.erl
@@ -0,0 +1,142 @@
+%%%-------------------------------------------------------------------
+%% @doc ex_banking public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(ex_banking).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+-export([create_user/1,deposit/3,withdraw/3,get_balance/2,send/4]).
+-export([get_currency/1,add_currency/2,remove_currency/1,update_currency/2]).
+-define(FU(K,V),proplists:get_value(K, V)).
+
+%----------------------------------------------------------------------------------
+%--------------------Banking  API--------------------------------------------------
+%----------------------------------------------------------------------------------
+% 
+-spec create_user(User :: string())-> ok | {error, wrong_arguments} | user_already_exists.
+create_user(User) when not is_list(User) , not is_atom(User)->{error,wrong_arguments};
+create_user(User)->
+    {ok,Pid}=ex_banking_client_sup:fetch_worker(),
+    Result= ex_banking_client:create_user(Pid, User),
+    Result.
+           
+
+
+
+-spec get_balance(User :: string(), Currency :: string()) 
+                  -> {ok, Balance::number()} | {error, wrong_arguments }| 
+                        user_does_not_exist | too_many_requests_to_user.
+get_balance(User,Currency)->
+    {ok,Pid}=ex_banking_client_sup:fetch_worker(),
+    {ok,Balance}= ex_banking_client:get_balance(Pid,{User,Currency}),
+    {ok,Balance}.
+            
+
+
+
+-spec deposit(User :: string(),
+              Amount :: number(), 
+              Currency :: string())->  {ok, New_balance::number()} |
+                                       {error,wrong_arguments} | 
+                                       user_does_not_exist | 
+                                       too_many_requests_to_user.
+deposit(_User,Amount,Currency) when 
+                not is_number(Amount) or 
+                (is_number(Amount) andalso Amount<0) or
+                not (is_atom(Currency) or is_list(Currency))->{error,wrong_arguments};
+
+
+deposit(User,Amount,Currency)->
+    {ok,Pid}=ex_banking_client_sup:fetch_worker(),
+    Result= ex_banking_client:deposit(Pid, {User,Amount,Currency}),
+    Result.
+           
+
+
+
+
+-spec withdraw( User :: string(), Amount :: number(), Currency :: string()) -> 
+                                       {ok, New_balance :: number()} |
+                                       {error, wrong_arguments} | 
+                                       user_does_not_exist | 
+                                       not_enough_money | 
+                                       too_many_requests_to_user.
+withdraw(_User,Amount,Currency) when 
+                            not is_number(Amount) or 
+                            not (is_atom(Currency) or is_list(Currency))->{error,wrong_arguments};
+                    
+withdraw(User,Amount,Currency)->
+    {ok,Pid}=ex_banking_client_sup:fetch_worker(),
+    Result=ex_banking_client:withdraw(Pid,{User,Amount,Currency}),
+    Result.
+            
+
+
+-spec send(From_User :: string(), To_User :: string(), Amount :: number(), Currency :: string()) ->
+                                     {ok, From_User_Balance :: number(), To_User_Balance :: number()} |
+                                     {error, wrong_arguments} | not_enough_money | sender_does_not_exist | 
+                                     receiver_does_not_exist | too_many_requests_to_sender | 
+                                     too_many_requests_to_receiver.
+send(_From_User,_To_User,Amount,Currency) when 
+                not is_number(Amount) or 
+                not (is_atom(Currency) or is_list(Currency))->{error,wrong_arguments};
+            
+send(From_User,To_User,Amount,Currency)->
+    {ok,Pid}=ex_banking_client_sup:fetch_worker(),
+    Result=ex_banking_client:send(Pid,{From_User,To_User,Amount,Currency}),
+    Result.
+
+%----------------------------------------------------------------------------------
+%--------------------Currency API-------------------------------------------------
+%----------------------------------------------------------------------------------
+get_currency(Currency)->
+    ex_banking_currency_server:get_coefficient(Currency).
+add_currency(Currency,Coefficient)->
+    ex_banking_currency_server:add_currency(Currency, Coefficient).
+
+remove_currency(Currency)->
+    ex_banking_currency_server:remove_currency(Currency).
+
+update_currency(Currency,Coefficient)->
+    ex_banking_currency_server:update_currency(Currency, Coefficient).
+
+
+
+
+   
+start({takeover,Node},Args)->
+    
+    io:format("taking over to node ~p",[Node]),
+    ex_banking_sup:start_link();
+start(_StartType, _StartArgs)->
+    io:format("Starting non-distributed"),
+    connect_to_cluster(),
+    ex_banking_sup:start_link().
+stop(_State) ->
+    ok.
+
+get_nodes()->
+    {ok,Env}=application:get_env(kernel,distributed),
+    AllNodes=proplists:get_value(ex_banking, Env),
+    MandatoryNodes=proplists:get_value(sync_nodes_mandatory,Env),
+    {ok,{AllNodes,MandatoryNodes}}.
+    
+connect_to_cluster()->
+     {ok,{AllNodes,MandatoryNodes}}=get_nodes(),
+     ping_nodes(MandatoryNodes),
+     io:format("All: ~p   Mandatory:~n~p",[AllNodes,MandatoryNodes]).
+    %  Targets=lists:filter(fun({Name,Importance})->Name =/=node() andalso Importance=:=primary  end, Nodes),
+    %  io:format("~p",[Targets]),
+    %  ping_nodes(Targets).
+     
+
+ping_nodes(List)->
+    Results=[net_adm:ping(Name)||Name<-List],
+    case lists:any(fun(Elem)->Elem =/= pong end ,Results) of
+            true -> throw({error,connect_to_nodes});
+            false-> ok
+    end.
+%% internal functions
---
+++ prod1/ex_banking/lib/ex_banking-0.1.0/src/ex_banking_currency_server.erl
@@ -0,0 +1,98 @@
+-module(ex_banking_currency_server).
+-behaviour(gen_server).
+-export([start_link/0,init/1]).
+-export([handle_call/3,handle_cast/2]).
+-export([get_coefficient/1,add_currency/2,remove_currency/1,update_currency/2]).
+
+-define(NAME,?MODULE).
+-record(state,{
+    currencies
+}).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         API    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+start_link()->
+    gen_server:start_link({local,?NAME}, ?MODULE, [],[]).
+
+init(_Args)->
+    {ok,Value}=application:get_env(ex_banking, base_currency),
+    {ok,#state{currencies=dict:store(Value, 1, dict:new())}}.
+
+
+-spec get_coefficient(Currency)->{ok,{Currency, Coefficient::number()}} | currency_not_found |  {error , wrong_arguments} 
+                                    when Currency:: list() | atom().
+get_coefficient(Currency) when not is_list(Currency) , not is_atom(Currency)->
+    {error,invalid_arguments};
+get_coefficient(Currency)->
+    gen_server:call(?NAME, {get_currency,Currency}).
+
+
+
+-spec add_currency(Currency, Coefficient::number())->{ok,{added,Currency}} | 
+                                                      currency_already_exists |{error , wrong_arguments}
+                                                      when Currency :: list() | atom() .
+add_currency(Currency,Coefficient) when not is_number(Coefficient) ;not Coefficient>0; not (is_list(Currency) or is_atom(Currency))  ->
+    {error,invalid_arguments};
+
+add_currency(Currency,Coefficient)->
+    gen_server:call(?NAME, {add_currency,Currency,Coefficient}).
+
+
+
+-spec remove_currency(Currency)->{ok,{removed,Currency}}  | {error , wrong_arguments}
+                                    when Currency:: list() | atom().
+remove_currency(Currency) when not is_atom(Currency) ; not is_list(Currency) ->
+    {error,invalid_arguments};
+
+remove_currency(Currency)->
+    gen_server:call(?NAME, {remove_currency,Currency}).
+
+
+
+-spec update_currency(Currency, Coefficient::number())->{ok,{updated,Currency}} |
+                                                         currency_does_not_exist | {error , wrong_arguments}
+                                                         when Currency:: list() | atom().
+update_currency(Currency,Coefficient) when not is_number(Coefficient) ; not Coefficient>0 ; not is_list(Currency) , not is_atom(Currency) ->
+    {error,wrong_arguments};
+
+update_currency(Currency,Coefficient)->
+    gen_server:call(?NAME, {update_currency,Currency,Coefficient}).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         Handlers    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+handle_cast(stop,State)->
+    {stop,State}.
+handle_call({get_currency,Currency},_From,State)->
+    Reply=case dict:find(Currency, State#state.currencies) of
+            error ->currency_does_not_exist;
+            {ok,Value}->{ok,Value}
+    end,
+    {reply,Reply,State};
+handle_call({add_currency,Currency,Coefficient},_From,State)->
+    case dict:find(Currency, State#state.currencies) of
+            {ok,_Value}->{reply,currency_already_exists,State};
+            error -> NewDict=dict:store(Currency,Coefficient,State#state.currencies),
+                     {reply,{ok,{added,Currency}},State#state{currencies=NewDict}}
+            
+    end;
+    
+
+handle_call({remove_currency,Currency},_From,State)->
+    NewDict=dict:erase(Currency, State),
+    {reply,{ok,{removed,Currency}},State#state{currencies=NewDict}};
+
+
+handle_call({update_currency,Currency,Coefficient},_From,State)->
+   case dict:find(Currency, State#state.currencies) of
+                error ->{reply,currency_does_not_exist,State};
+                {ok,_}-> NewDict=dict:store(Currency, Coefficient,State#state.currencies),
+                         {reply,{ok,{updated,Currency}},State#state{currencies=NewDict}}
+    end.
+   
