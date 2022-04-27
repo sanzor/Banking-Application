02d5e1479ede1f02c2d440ed0ebbf45db4bbbb7f
Adrian Bercovici <adriansan_93@yahoo.com>
2022-04-28 01:33:35 +0300

    finally fixed

--- relscript1.sh
+++ relscript1.sh
@@ -5,5 +5,5 @@ port=${2:-8300}
 release_name=${3:- "ex_banking"}
 folder=${4:- "./prod1/ex_banking/bin/ex_banking"}
 echo "Shell : Using node: $node on port: $port for release: $release_name"
-rebar3 release as prod1 -o prod1
+rebar3 as prod1 release -o prod1 
 RELX_REPLACE_OS_VARS=true NODE_NAME=${node} XX=33 PORT=${port} $folder console
