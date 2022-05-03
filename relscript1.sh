b3bb2e7f37494e13d41d9aedf5539f1b1d13dc9d
Adrian Bercovici <adriansan_93@yahoo.com>
2022-05-03 22:36:49 +0300

    added flag

--- relscript1.sh
+++ relscript1.sh
@@ -1,6 +1,6 @@
 
 release_name=${3:- "ex_banking"}
-node="${1:-"a"}"
+node="${1:- "a"}"
 port=${2:-8300}
 release_name=${3:- "ex_banking"}
 folder=${4:- "./prod1/ex_banking/bin/ex_banking"}
@@ -8,4 +8,5 @@ echo "Using node: $node on port: $port for release: $release_name"
 rebar3 as prod1 release -o prod1
 echo "Node name passing is:\n"
 echo "${node}"
+echo "${port}"
 NODE_NAME=${node}  PORT=${port} $folder console
