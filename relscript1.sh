ea82ea30eb7f0e5e4bea216aecfbe3a26f585f48
adrian <adriansan_93@yahoo.com>
2022-05-08 22:22:36 +0300

    commit

--- relscript1.sh
+++ relscript1.sh
@@ -5,7 +5,7 @@ port=${2:-8300}
 release_name=${3:- "ex_banking"}
 folder=${4:- "./prod1/ex_banking/bin/ex_banking"}
 echo "Using node: $node on port: $port for release: $release_name"
-rebar3 as prod1 release -o prod1
+rebar3 as prod release -o prod1
 echo "Node name passing is:\n"
 echo "${node}"
 echo "${port}"
