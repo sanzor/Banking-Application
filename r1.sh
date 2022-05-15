a7e8e2d2ef4803c7d6a86d80c5a7cbaf592fb0c3
Adrian Bercovici <adriansan_93@yahoo.com>
2022-05-15 15:57:00 +0300

    distribution works

--- r1.sh
+++ r1.sh
@@ -1,13 +1,12 @@
 
 release_name=${3:- "ex_banking"}
-node="'${1:-"a"}@DESKTOP-GOMS8S8'"
+node="'${1:-"a"}@127.0.0.1'"
 port=${2:-8300}
 release_name=${3:- "ex_banking"}
 folder=${4:- "./prod1/ex_banking/bin/ex_banking"}
 echo "Using node: $node on port: $port for release: $release_name"
-rebar3 compile
-rebar3 as prod release -o prod1
+rebar3 as prod1 release -o prod1
 echo "Node name passing is:"
 echo "${node}"
 echo "${port}"
-RELX_REPLACE_OS_VARS=true NODE_NAME="${node}"  PORT=${port} $folder console 
+RELX_REPLACE_OS_VARS=true NODE_NAME=${node}  PORT=${port} $folder console 
--- r2.sh
+++ r2.sh
@@ -1,11 +1,11 @@
 
 release_name=${3:- "ex_banking"}
-node="'${1:-"b"}@DESKTOP-GOMS8S8'"
+node="'${1:-"b"}@127.0.0.1'"
 port=${2:-8300}
 release_name=${3:- "ex_banking"}
 folder=${4:- "./prod2/ex_banking/bin/ex_banking"}
 echo "Using node: $node on port: $port for release: $release_name"
-rebar3 as prod release -o prod2
+rebar3 as prod2 release -o prod2
 echo "Node name passing is:\n"
 echo "${node}"
 echo "${port}"
